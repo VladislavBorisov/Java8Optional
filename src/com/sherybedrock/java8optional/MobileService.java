6474dfebd53414ad5df79b93d73eb860514d6937
Vladislav.Borisov <Vladislav.Borisov@LTBPSOF162.bpsf.bulpros.com>
2017-03-08 17:16:39 +0200

    Java 8 Optional Example

---
+++ src/com/sherybedrock/java8optional/MobileService.java
@@ -0,0 +1,13 @@
+package com.sherybedrock.java8optional;
+
+import java.util.Optional;
+
+public class MobileService {
+
+	public Integer getMobileScreenWidth(Optional<Mobile> mobile) {
+		return mobile.flatMap(Mobile::getDisplayFeatures)
+				.flatMap(DisplayFeatures::getResolution)
+				.map(ScreenResolution::getWidth).orElse(0);
+	}
+
+}
